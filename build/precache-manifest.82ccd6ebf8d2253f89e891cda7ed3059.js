4ea14e504011021781e468271191ac557c2a38c8
Chris Carreon <mrwiggles6545@yahoo.com>
2020-09-18 19:00:53 -0500

    build pkg

---
+++ build/precache-manifest.82ccd6ebf8d2253f89e891cda7ed3059.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ba8d13c88f470430ac6cc46d0183dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4baf83a0f9f45a181dc",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "d4baf83a0f9f45a181dc",
+    "url": "/static/js/2.4c432541.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c432541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b83a0f5f8785e772acc",
+    "url": "/static/js/main.737a65e3.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
